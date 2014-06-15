19c8bcff51b9a1540e28674cebfa3d25cc425fe1
René Wilhelm <rene.wilhelm@gmail.com>
2014-06-15 18:33:15 +0200

    Minor fixes

--- Gulpfile.js
+++ Gulpfile.js
@@ -41,14 +41,14 @@ gulp.task('sass', function () {
       style: process.env.NODE_ENV === 'development' ? 'expanded' : 'compressed'
     }))
     .pipe(autoprefixer("last 1 version", "> 1%", "ie 8", "ie 7"))
-    .pipe(gulp.dest('public/css/'));
+    .pipe(gulp.dest('public/css'));
 });
 
 // watch stuff for changes
 gulp.task('watch', function () {
   gulp.watch(['public/css/**/*.scss'], ['sass']);
   gulp.watch(['public/js/src/**/*.jsx'], ['react']);
-  gulp.watch(['public/html/src/**/*.jade'], ['react']);
+  // gulp.watch(['public/html/src/**/*.jade'], ['jade']);
 });
 
 // start the server using nodemon (so it restarts if neccessary)
@@ -94,6 +94,7 @@ gulp.task('server', function () {
 // development mode: compile sass and react files once, then watch them for
 // changes and start the server on port 4001
 gulp.task('default', ['sass', 'jade', 'react', 'watch', 'server']);
+gulp.task('default', ['sass', 'react', 'watch', 'server']);
 
 // same as above, but don't watch anything
 gulp.task('production', ['sass', 'react', 'server']);
