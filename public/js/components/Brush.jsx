20e679746a44fc830d13a8816deb340730ab474a
René Wilhelm <rene.wilhelm@gmail.com>
2014-06-24 06:24:09 +0200

    Bugfixed Brush and Chart

--- public/js/components/Brush.jsx
+++ public/js/components/Brush.jsx
@@ -12,13 +12,25 @@ var Brush = React.createClass({
 
   loadMoreData: function(extent) {
     // this.setState({ extent: extent });
-    console.log('Brush.jsx:loadMoreData', extent);
+    console.log('Brush.jsx:loadMoreData' + this.props.sensor);
     this.props.loadMoreData(extent);
     // return this.props.loadMoreData(extent, this.props.extent);
   },
 
-	componentDidMount: function() {
+  componentWillReceiveProps: function(nextProps) {
+    console.log('Brush.jsx:componentWillReceiveProps' + this.props.sensor);
+  },
 
+  componentWillUpdate: function(nextProps, nextState) {
+    console.log('Brush.jsx:componentWillUpdate' + this.props.sensor);
+  },
+
+  // brush: function() {
+  //   var brush = d3.svg.brush();
+
+  // },
+  
+	componentDidMount: function() {
     var xScale = d3.time.scale()
       .range([0, this.props.size.width]);
 
@@ -29,13 +41,11 @@ var Brush = React.createClass({
     yScale.domain(this.props.yDomain);
 
 		var brush = d3.svg.brush();
-
     var chart = d3.select(this.getDOMNode());
 
     brush.x(xScale)
       .on("brushend", this.brushended.bind(this, brush));
       // .on("brushend", this.brushended.bind(this, brush));
-
 
     if (this.props.extent.length) {
       brush.extent(this.props.extent);
@@ -53,7 +63,7 @@ var Brush = React.createClass({
 
 
 	componentWillReceiveProps: function(nextProps) {
-		console.log('Brush.jsx:componentWillReceiveProps', this.props, nextProps);
+		console.log('Brush.jsx:componentWillReceiveProps:' + this.props.sensor);
 		// this.setState({
 		// 	extent: nextProps.extent
 		// })
@@ -61,12 +71,13 @@ var Brush = React.createClass({
 
 
 	componentWillUpdate: function(nextProps, nextState) {
-		console.log('Brush.jsx:componentWillUpdate', this.props, nextProps);
+		console.log('Brush.jsx:componentWillUpdate:' + this.props.sensor);
 	},
 
 
 	componentDidUpdate: function(prevProps, prevState) {
-		console.log('Brush.jsx:componentDidUpdate', this.props);
+		console.log('Brush.jsx:componentDidUpdate:' + this.props.sensor);
+    d3.selectAll('chart').select('.brush').call(brush.clear());
 	},
 
 
@@ -74,14 +85,13 @@ var Brush = React.createClass({
   	// this.props.extent = brush.empty() ? [] : brush.extent().map(function(d) { return +d; });
     // loadMoreData(brush.empty() ? [] : brush.extent().map(function(d) { return +d; }));
     // this.loadMoreData(brush.extent().map(function(d) { return +d; }));
-    this.props.loadMoreData(brush.empty() ? [] : brush.extent().map(function(d) { return +d; }));
+    this.loadMoreData(brush.empty() ? [] : brush.extent().map(function(d) { return +d; }));
     // this.props.extent.bind(this) = brush.empty() ? [] : brush.extent().map(function(d) { return +d; });
     // this.props.extent.bind(this) = brush.empty() ? [] : brush.extent().map(function(d) { return +d; });
     // console.log('Brush.jsx:brushended': this.props.extent);
     // this.loadMoreData.bind(this, brush.empty() ? [] : brush.extent().map(function(d) { return +d; }), brush.extent().map(function(d) { return +d; }));
     // that.setState({extent: brush.empty() ? [] : brush.extent().map(function(d) { return +d; })});
     // dispatch.brushended(brush.empty() ? [] : brush.extent().map(function(d) { return +d; }));
-    // d3.selectAll('chart').select('.brush').call(brush.clear());
   },
 
 	render: function() {
--- public/js/components/Chart.jsx
+++ public/js/components/Chart.jsx
@@ -7,43 +7,42 @@ var Chart = React.createClass({
     // offset.y: React.PropTypes.number.isRequired,
     // offset.x: React.PropTypes.number.isRequired
   },
-  componentDidMount: function() {
-    console.info('Chart:componentDidMount', this.props.extent);
-    var xScale = d3.time.scale()
-      .range([0, this.props.size.width]);
 
-    var yScale = d3.scale.linear()
-      .range([this.props.size.height, 0]);
+  getDefaultProps: function() {
+    var xScale = d3.time.scale().range([0, this.props.size.width]);
+    var yScale = d3.scale.linear().range([this.props.size.height, 0]);
 
-    xScale.domain(this.props.extent.length ? this.props.extent : this.props.xDomain);
-    yScale.domain(this.props.yDomain);
+    return {
+      xDomain: xScale.domain(this.props.extent.length ? this.props.extent : this.props.xDomain),
+      yDomain: yScale.domain(this.props.yDomain),
+      xAxis: d3.svg.axis().scale(xScale).orient("bottom").ticks(d3.time.minutes, 1),
+      yAxis: d3.svg.axis().scale(yScale).orient("left").ticks(Math.max(this.props.size.height / 25, 2)),
+    };
+  },
 
-    var xAxis = d3.svg.axis().scale(xScale)
-      .orient("bottom")
-      .ticks(d3.time.minutes, 1);
-
-    var yAxis = d3.svg.axis().scale(yScale)
-      .orient("left")
-      .ticks(Math.max(this.props.size.height / 25, 2));
-
-    var chart = d3.select(this.getDOMNode().children[0]);
+  appendAxis: function(domNode) {
+    domNode.selectAll('.axis').remove();
 
     // x-axis
-    chart
+    domNode
       .append('g')
         .attr("class", "x axis")
         .attr("transform", this.props.offset.x)
-        .call(xAxis);
+        .call(this.props.xAxis);
 
     // y-axis
-    chart
+    domNode
       .append("g")
         .attr("class", "y axis")
         .attr("transform", this.props.offset.y)
-        .call(yAxis);
+        .call(this.props.yAxis);
+  },
+
+  appendLabels: function(domNode) {
+    domNode.selectAll('.label').remove();
 
     // x-label
-    chart
+    domNode
       .append("text")
         .attr("class", "x label")
         .attr("text-anchor", "end")
@@ -52,19 +51,43 @@ var Chart = React.createClass({
         .text(this.props.axisLabel.xAxis.text);
 
     // y-label
-    chart
-    .append("text")
-      .attr("class", "y label")
-      .attr("text-anchor", "start")
-      .attr("x", this.props.axisLabel.yAxis.x)
-      .attr("y", this.props.axisLabel.yAxis.y)
-      .text(this.props.axisLabel.yAxis.text);
+    domNode
+      .append("text")
+        .attr("class", "y label")
+        .attr("text-anchor", "start")
+        .attr("x", this.props.axisLabel.yAxis.x)
+        .attr("y", this.props.axisLabel.yAxis.y)
+        .text(this.props.axisLabel.yAxis.text);
+  },
+
+  appendClipPath: function(domNode) {
+    domNode.selectAll('#clip-path').remove();
+
+    domNode
+      .append("clipPath")
+        .attr("id", "clip")
+        .append("rect")
+          .attr("width", this.props.size.width)
+          .attr("height", this.props.size.height);
+
+  },
+
+  componentDidMount: function() {
+    console.info('Chart:componentDidMount' + this.props.sensor);
+    var svg = d3.select(this.getDOMNode());
+    var chart = d3.select(this.getDOMNode().children[0]);
+    this.appendAxis(chart);
+    this.appendLabels(chart);
+    this.appendClipPath(svg);
   },
   componentWillUpdate: function(nextProps, nextState) {
-    console.log('Chart.jsx:componentWillUpdate', nextProps, nextState);
+    console.log('Chart:componentWillUpdate' + this.props.sensor);
+    var chart = d3.select(this.getDOMNode().children[0]);
+    this.appendAxis(chart)
+    this.appendLabels(chart);
   },
   render: function() {
-    console.info('Chart:render', this.props);
+    console.info('Chart:render' + this.props.sensor);
 
     var offset = this.props.offset;
     var size = this.props.size;
