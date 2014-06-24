03830dffecdbb5b3f7d189dd9ce706c20c209091
René Wilhelm <rene.wilhelm@gmail.com>
2014-06-25 01:26:53 +0200

    Simplified raw-directive.js

--- public/js/directives/raw-directive.js
+++ public/js/directives/raw-directive.js
@@ -31,62 +31,17 @@
             );
           }
 
-          // function renderComponent() {
-          //   React.renderComponent(
-          //     SensorChart({
-          //       sensor: $scope.sensor,
-          //       data  : $scope.trip.sensorData.sensors[$scope.sensor],
-          //       extent: $scope.trip.sensorData.extent,
-          //       xDomain: $scope.trip.sensorData.xDomain,
-          //       yDomain: $scope.trip.sensorData.yDomain,
-          //       height: 200, // $element[0].offsetHeight || 200, // FIXME
-          //       width: $element[0].offsetWidth,
-          //       loadMoreData: function(extent, oldExtent) {
-          //         console.log('chartDirective:loadMoreData', extent, oldExtent);
-          //         $scope.loadMoreData({extent: extent, oldExtent: oldExtent});
-          //       },
-          //       // updateExtent: function(extent) {
-          //       //   console.log('chartDirective:updateExtent');
-          //       //   $scope.updateExtent({extent: extent});
-          //       // },
-          //       // onChange: handleChanges(function(extent) {
-          //       //   $scope.trip.data.extent = extent;
-          //       //   $scope.$apply();
-          //       // }
-          //     }), $element[0]
-          //   );
-          // }
-
-          function tripIsReady () {
+          var tripIsReady = function() {
             if (!$scope.trip) return false;
+            if (!$scope.trip.data) return false;
+            // if (!$scope.trip.data.arrayLengthSum <= 600) return false;
+            return true;
+          };
 
-            var dataCount =
-              Object.keys($scope.trip.sensorData.sensors)
-                .map(function(sensor) {
-                  return $scope.trip.sensorData.sensors[sensor].length;
-                })
-                .reduce(function(a,b){ return a + b; });
-
-            var domainsReady =
-              $scope.trip.sensorData.xDomain.length + $scope.trip.sensorData.yDomain.length;
-
-            return (
-              $scope.trip &&
-              dataCount <= 600 &&
-              domainsReady == 4 // uh oh
-            );
-          }
-
-          $scope.$watchCollection('trip.sensorData', function(val, oldVal) {
+          $scope.$watchCollection('trip.data', function(val, oldVal) {
             if (tripIsReady()) {
-              if (val.extent.length) {
-                if (val.extent !== oldVal.extent) {
-                  console.log('rawDirective:$watch', val.extent, oldVal.extent);
-                  renderComponent();
-                }
-              } else {
-                renderComponent();
-              }
+              console.log('rawDirective:$watch', val, oldVal);
+              renderComponent();
             }
           });
         }
