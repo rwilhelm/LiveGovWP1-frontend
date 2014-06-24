784ce4ddb3211d076d8ecd5a383a684b97f920de
René Wilhelm <rene.wilhelm@gmail.com>
2014-06-24 06:23:24 +0200

    ChartInfo React component

--- public/js/components/ChartInfo.jsx
+++ public/js/components/ChartInfo.jsx
@@ -1,1 +1,15 @@
 /*** @jsx React.DOM */
+
+var ChartInfo= React.createClass({
+	componentWillReceiveProps: function(nextProps) {
+		console.log('ChartInfo:componentWillReceiveProps' + this.props.sensor);
+	},
+	render: function() {
+		var style = {
+			y: -30
+		};
+		return (
+			<div style={style}>EXTENT: {this.props.extent[0]}, {this.props.extent[1]}</div>
+		);
+	}
+});
